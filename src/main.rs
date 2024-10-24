78927cf355ac94832ea4a3ff1738d56906627e0f
yanic <yanic.dev@gmail.com>
2024-10-24 19:52:34 +0200

    finish 1.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Cli = Cli::parse();
+
+    println!("pattern: {:?}\n path: {:?}", args.pattern, args.path);
 }
